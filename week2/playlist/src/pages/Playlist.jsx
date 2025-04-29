e7f0ff9ae394a243e6bdb2f176ceea833e4b9cf6
chan000518 <wkdvnddl050701@gmail.com>
2025-04-29 21:00:31 +0900

    session8.1)

--- week2/playlist/src/pages/Playlist.jsx
+++ week2/playlist/src/pages/Playlist.jsx
@@ -5,7 +5,7 @@ export const Playlist = () => {
   return (
     <main className="p-10">
       <section className="flex flex-wrap gap-8 justify-center">
-        {albumData.map((album, index) => ( // 키 값을 배열의 인덱스로 키: 컴포넌트가 중복될 때 
+        {albumData.map((album, index) => ( // 키 값을 배열의 인덱스로 키: 컴포넌트가 중복될 때  구분을 위해 
           <Album key={index} album={album} />
         ))}
       </section>
