9c0ab7aa8eb671212e570c384db2fd12b09a4383
chan000518 <wkdvnddl050701@gmail.com>
2025-05-02 16:42:45 +0900

    session10.2)

--- week2_study/session08/src/App.jsx
+++ week2_study/session08/src/App.jsx
@@ -10,37 +10,70 @@ const mockData = [
   {id: 2, isDone: false, content: "노래연습하기", date: new Date().getTime()},
 ]
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.newTodo, ...state];
+    case 'UPDATE':
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+};
+
 function App() {
   const [todos, setTodos] = useState(mockData);
   const idRef = useRef(3);
 
+//   const onCreate = (content) => {
+//     const newTodo = {
+//       id: idRef.current,
+//       isDone: false,
+//       content : content,
+//       date: new Date().getTime(),
+//     }
+//     setTodos([newTodo,...todos]) // 상태 변화 함수
+//   }
+
+//   const onUpdate = (targetId) => {
+//   //   setTodos(todos.map((todo) => {
+//   //   if(todo.id === targetId){
+//   //     return {
+//   //       ...todo,
+//   //       isDone: !todos.isDone
+//   //     }
+//   //   }
+//   //   return todo;
+//   // }))
+//   setTodos(todos.map((todo) => todo.id === targetId ? {...todo, isDone: !todo.isDone} : todo));
+//   }
+
+//   const onDelete = (targetId) => {
+//     setTodos(todos.filter((todo) => todo.id !== targetId));
+//   }
+
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current,
-      isDone: false,
-      content : content,
-      date: new Date().getTime(),
-    }
-    setTodos([newTodo,...todos]) // 상태 변화 함수
-  }
+    dispatch({ 
+      type: 'CREATE',
+      data: { 
+        id: idRef.current++, 
+        isDone: false, 
+        content, 
+        date: new Date().getTime(), 
+      } });
+  };
 
   const onUpdate = (targetId) => {
-  //   setTodos(todos.map((todo) => {
-  //   if(todo.id === targetId){
-  //     return {
-  //       ...todo,
-  //       isDone: !todos.isDone
-  //     }
-  //   }
-  //   return todo;
-  // }))
-  setTodos(todos.map((todo) => todo.id === targetId ? {...todo, isDone: !todo.isDone} : todo));
-}
+    dispatch({ type: 'UPDATE', targetId });
+  };
 
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
-  }
-
+    dispatch({ type: 'DELETE', targetId });
+  };
   return (
     <div className="App">
       <Header />
